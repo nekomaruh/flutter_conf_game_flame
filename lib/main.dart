a06d5bb64f84351a3cc3f2cc2dc5c0b8b5814d7d
Johan Esteban <johesteb@gmail.com>
2024-10-30 16:00:20 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,3 @@ void main() {
   final gameWidget = GameWidget(game: GameWorld());
   runApp(gameWidget);
 }
-
-/*
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: GameWorld()
-    );
-  }
-}
-*/
--- lib/player/player.dart
+++ lib/player/player.dart
@@ -1,3 +1,29 @@
-class Player {
-  
+import 'dart:async';
+
+import 'package:flame/components.dart';
+
+class Player extends SpriteAnimationGroupComponent with HasGameRef {
+  @override
+  Future<void> onLoad() async {
+    await _loadAnimation();
+    size = Vector2(128, 128);
+    position = Vector2(100, 720/2);
+  }
+
+  _loadAnimation() async {
+    final idle = [await game.loadSprite('personaje/Personaje-F0.png')];
+    final run = [
+      await game.loadSprite('personaje/Personaje-F1.png'),
+      await game.loadSprite('personaje/Personaje-F2.png'),
+      await game.loadSprite('personaje/Personaje-F3.png'),
+      await game.loadSprite('personaje/Personaje-F4.png'),
+      await game.loadSprite('personaje/Personaje-F5.png')
+    ];
+    animations = {
+      'idle': SpriteAnimation.spriteList(idle, stepTime: 0.1),
+      'run': SpriteAnimation.spriteList(run, stepTime: 0.1)
+    };
+    current = 'idle';
+    //final sprites = SpriteSheet(image: image, srcSize: srcSize);
+  }
 }
